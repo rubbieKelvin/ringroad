0c46e92a8b576d5e9a77221a7665ddd4b6078125
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-10-22 09:03:36 +0100

    chore: added fe

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+/**
+ * main.ts
+ *
+ * Bootstraps Vuetify and other plugins then mounts the App`
+ */
+
+// Components
+import App from './App.vue'
+
+// Composables
+import { createApp } from 'vue'
+
+// Plugins
+import { registerPlugins } from '@/plugins'
+
+import './styles/index.scss'
+
+const app = createApp(App)
+
+registerPlugins(app)
+
+app.mount('#app')
