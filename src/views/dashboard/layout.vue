8f465ee827159bf7f4d224e0f6f7b36a7d5a61c2
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-10-24 11:39:34 +0100

    chore: dashboard home

--- src/views/dashboard/layout.vue
+++ src/views/dashboard/layout.vue
@@ -1,7 +1,22 @@
 <template>
-    <div>
-      <p>Dashboard</p>
-      <RouterView />
+  <div>
+    <p>Dashboard</p>
+    <div class="flex gap-3">
+      <RouterLink to="#" v-for="i in links" :key="i">
+        {{ i }}
+      </RouterLink>
     </div>
-  </template>
-  
+    <RouterView />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const links = [
+  "Home",
+  "Items",
+  "Suppliers",
+  "Manage categories",
+  "Staffs",
+  "Settings",
+];
+</script>
