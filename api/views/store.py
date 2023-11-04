bb3b663d9fab1df9d3bf5927fa2720d3801bfcf0
Ngorhzee <ngoziamolo02@gmail.com>
2023-11-04 16:27:11 +0100

    chore: modified  views/store.py

--- api/views/store.py
+++ api/views/store.py
@@ -8,7 +8,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status, serializers
-
+from uuid import UUID
 from shared.view_tools import exceptions
 from shared.view_tools import body_tools
 from shared.view_tools.paths import Api
@@ -16,63 +16,73 @@ from shared.view_tools.paths import Api
 
 store_api = Api("store/", name="Store")
 
-
-# TODO: review
-class UpdateStoreSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Store
-        fields = ["name", "description"]
-
-
 class CreateStoreInput(pydantic.BaseModel):
     store_name: str
     store_description: str
 
-
-@store_api.endpoint("create", method="POST", permission=IsAuthenticated)
+@store_api.endpoint("create", method="POST",permission=IsAuthenticated)
 @body_tools.validate(CreateStoreInput)
 def create_store(request: Request) -> Response:
-    data: CreateStoreInput = body_tools.get_validated_body(request=request)
+    data: CreateStoreInput = body_tools.get_validated_body(request)
     user: User = typing.cast(request.user, User)
 
-    store = Store.objects.create(
-        store_name=data.store_name, store_description=data.store_description, owner=user
-    )
+    store = Store.objects.create(store_name=data.store_name, store_description=data.store_description, owner=user,)
 
     return Response(status=status.HTTP_201_CREATED)
 
+class UpdateStoreInput(pydantic.BaseModel):
+    store_name: str | None
+    store_description: str | None
 
-@store_api.endpoint("update-store/<id>", method="PATCH", permission=IsAuthenticated)
-@body_tools.validate(CreateStoreInput)
+@store_api.endpoint("update<id>", method="PATCH", permission=IsAuthenticated)
+@body_tools.validate(UpdateStoreInput)
 def update_store(request: Request, id: str) -> Response:
-    data: CreateStoreInput = body_tools.get_validated_body(request=request)
-    user: User = typing.cast(request.user, User)
-
+    data: UpdateStoreInput = body_tools.get_validated_body(request)
+    if not data.store_name and not data.store_description:
+        raise exceptions.ApiException("Pass in atlest one")
+    
+    checkUUID(id=id, value="Store")
+    
     try:
-        store = Store.objects.get(id=id)
-        if store:
-            # TODO: review
-            store_update_data = UpdateStoreSerializer(instance=user, data=request.data, partial=True)  # type: ignore
-            if store_update_data.is_valid():
-                store_update_data.save()
-                return Response(status=status.HTTP_201_CREATED)
-
-            else:
-                return Response("invalid data format")
-        else:
-            return Response("store is not the right store")
-
+        store: Store = Store.objects.get(id=id)
+        
+        if data.store_name:
+            store.name = data.store_name
+            store.save()
+        
+        if data.store_description:
+            store.description = data.store_description
+            store.save()
+        return Response(store.serialize())
+    
     except Store.DoesNotExist:
-        raise exceptions.ApiException("store does not exist!")
+        raise exceptions.ResourceNotFound("Store does not exist")
+    
 
-
-@store_api.endpoint("delete/<id>", method="POST", permission=IsAuthenticated)
+@store_api.endpoint("delete<id>", method="DELETE", permission=IsAuthenticated)
 def delete_store(request: Request, id: str) -> Response:
-    user = request.user
+    checkUUID(id=id,value="Store")
 
     try:
-        store = Store.objects.get(id=id)
+        store: Store = Store.objects.get(id=id)
         store.delete()
-        return Response("successfully deleted")
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
     except Store.DoesNotExist:
-        raise exceptions.ResourceNotFound("Store not found")
+        raise exceptions.ResourceNotFound("Store does not exist")
+
+
+@store_api.endpoint("get", method="POST",permission=IsAuthenticated)
+def get_stores(request: Request) -> Response:
+    user: User = typing.cast(request.user, User)
+    stores = Store.objects.filter(owner=user)
+    return Response([
+        store.serialize() for store in stores
+    ])
+            
+def checkUUID(id: str, value: str):
+    try:
+        UUID(id)
+    except ValueError:
+        raise exceptions.ApiException(f"Invalid {value} Id")
+
