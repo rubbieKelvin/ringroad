bf18d3f36f0ad610f76b3fd7beb82bb6a541bec9
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-11-05 16:54:19 +0100

    chore: redefining api types

--- src/services/api/types.ts
+++ src/services/api/types.ts
@@ -1,20 +1,10 @@
+interface Operation<D, R, A, P> {
+  data: D
+  response: R
+  args: A
+  params: Partial<P>
+}
+
 export interface EndpointInterface {
-  "[PO] /api/auth/signup": {
-    data: {
-      first_name: string;
-      last_name: string;
-      email: string;
-      password: string;
-    };
-    response: {};
-    path_args?: {};
-  };
-  "[PO] /api/user/me": {
-    data: {};
-    response: {
-      id: string;
-      email: string;
-    };
-    path_args?: {};
-  };
+  
 }
