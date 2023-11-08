4e1c2d7b375f6612af6316b5da5eee9f38420a2b
rubbiekelvin <rubbiekelvinvoltsman@gmail.com>
2023-11-08 13:47:00 +0100

    chore: updated stores endpoints

--- api/models/store.py
+++ api/models/store.py
@@ -3,14 +3,23 @@ from .user import User
 from shared.abstractmodel import AbstractModel, serialization
 from shared.abstractmodel.serialization import SerializationStructure, struct
 
+
 class Store(AbstractModel):
-    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
-    description = models.CharField(max_length=50)
+    description = models.CharField(max_length=50, null=True)
+    is_public = models.BooleanField(default=False)
 
     @property
     def serializers(self) -> SerializationStructure:
-        return struct('id', 'date_created', 'name', 'description',owner=struct('id'),)
-    
+        return struct(
+            "id",
+            "date_created",
+            "name",
+            "is_public",
+            "description",
+            owner=struct("id"),
+        )
+
     def __str__(self):
         return self.name
